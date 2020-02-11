9d2983a86004d9114e3476f0a872aa9a8a463e58
Xin Liu <xhliu97@gmail.com>
2020-02-10 23:51:40 -0500

    updated readme for todo

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Material Design Components - https://material-ui.com/
+
+TODO:
+- Show list of results from query input
+- Users should be able to click on a item in the list
+- Images should be stored using GridFS and images should show
+- Comments should show with the document
+- Users should be able to use location to find data and the results should be the same as the word query
--- components/Spinner.js
+++ components/Spinner.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import {
+    Grid
+} from "@material-ui/core";
 
 /**
  * Refer to https://material-ui.com/components/backdrop/
@@ -7,9 +10,18 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 
 const Spinner = () => {
     return (
-        <div>
-            <CircularProgress color="inherit" />
-        </div>
+        <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justify="center"
+            style={{ minHeight: '100vh' }}
+        >
+            <Grid>
+                <CircularProgress size={80} color="inherit" />
+            </Grid>
+        </Grid>
     )
 }
 
