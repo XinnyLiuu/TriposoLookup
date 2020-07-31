4aa9c41e6bfb8051f815f42b14e1f7c833e5d134
Xin Liu <xhliu97@gmail.com>
2020-07-30 21:14:30 -0400

    updated lambda functions

--- next.config.js
+++ next.config.js
@@ -7,9 +7,9 @@
 module.exports = {
     env: {
         // The following are endpoints pointing to lambdas
-        get_location_by_id_api: "https://5a3yh57fa0.execute-api.us-east-1.amazonaws.com/dev/api/location/",
-        post_location_by_match_api: "https://5a3yh57fa0.execute-api.us-east-1.amazonaws.com/dev/api/location/match",
-        post_comment_api: "https://5a3yh57fa0.execute-api.us-east-1.amazonaws.com/dev/api/location/comment",
-        post_location_by_nearby_api: "https://5a3yh57fa0.execute-api.us-east-1.amazonaws.com/dev/api/location/nearby"
+        get_location_by_id_api: "https://8vs5zx1lxj.execute-api.us-east-1.amazonaws.com/dev/api/location/",
+        post_location_by_match_api: "https://8vs5zx1lxj.execute-api.us-east-1.amazonaws.com/dev/location/match",
+        post_comment_api: "https://8vs5zx1lxj.execute-api.us-east-1.amazonaws.com/dev/location/comment",
+        post_location_by_nearby_api: "https://8vs5zx1lxj.execute-api.us-east-1.amazonaws.com/dev/api/location/nearby"
     }
 }
